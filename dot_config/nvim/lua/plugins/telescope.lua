0fb86cf788cde04204b5871da604d6e550fbe0b4
Austin Thomas <austinredthomas@gmail.com>
2026-06-30 12:24:34 -0400

    feat(nvim): map Telescope picker navigation
    Configure Telescope defaults so Ctrl-j and Ctrl-k move to the next and previous picker result in both insert and normal modes.

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,24 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local actions = require("telescope.actions")
+
+        require("telescope").setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                    },
+                    n = {
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                    },
+                },
+            },
+        })
+    end,
     keys = {
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
         { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
